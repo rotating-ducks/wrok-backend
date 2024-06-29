6462f9b28805646ffb63a802d67a932e3e13647a
yg-moon <yg-moon@snu.ac.kr>
2024-06-29 22:23:05 +0900

    Change package name from 'com.example.wrok' to 'com.wrok.api'

--- src/main/java/com/wrok/api/WrokApplication.java
+++ src/main/java/com/wrok/api/WrokApplication.java
@@ -1,13 +1,13 @@
-package com.example.wrok;
+package com.wrok.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WrokApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(WrokApplication.class, args);
-	}
+    
+    public static void main(String[] args) {
+        SpringApplication.run(WrokApplication.class, args);
+    }
 
 }
--- src/test/java/com/wrok/api/WrokApplicationTests.java
+++ src/test/java/com/wrok/api/WrokApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.wrok;
+package com.wrok.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WrokApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
