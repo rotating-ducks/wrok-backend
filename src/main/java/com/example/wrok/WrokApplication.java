bc33d4930ebfec2ddbc47c48f96b1fb4a05be4ca
yg-moon <yg-moon@snu.ac.kr>
2024-06-29 19:44:29 +0900

    First commit from IntelliJ

---
+++ src/main/java/com/example/wrok/WrokApplication.java
@@ -0,0 +1,13 @@
+package com.example.wrok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrokApplication.class, args);
+	}
+
+}
